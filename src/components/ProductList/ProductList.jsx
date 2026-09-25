0e7b42952673a4c9afc204a87cbbd28a3dfb2ca2
Jeisson Uribe <ujeisson@gmail.com>
2026-09-24 20:40:00 -0500

    feat(ui): componente ProductList con grilla responsive

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,13 @@
+import ProductCard from '../ProductCard/ProductCard'
+
+function ProductList({ products }) {
+  return (
+    <div className="product-grid">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  )
+}
+
+export default ProductList
